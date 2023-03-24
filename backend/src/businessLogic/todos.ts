2d88c8c5ffdf380d07cf775f606b06d08247eba6
Kelechi Mbawuike <kmbawuike@gmail.com>
2023-03-24 10:54:13 +0100

    Implemented create todo

---
+++ backend/src/businessLogic/todos.ts
@@ -0,0 +1,35 @@
+import { TodoAccess } from '../dataLayer/todoAcess'
+import { AttachmentUtils } from '../helpers/attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+// import * as createError from 'http-errors'
+
+// TODO: Implement businessLogic
+const logger = createLogger('TodosAccess')
+const attachmentUtils = new AttachmentUtils()
+const todosAcess = new TodoAccess()
+
+// Write create todo function
+export async function createTodo(
+  newTodo: CreateTodoRequest,
+  userId: string
+): Promise<TodoItem> {
+  logger.info('Create todo function called')
+  const todoId = uuid.v4()
+  const createdAt = new Date().toISOString()
+  const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
+  const newItem = {
+    userId,
+    todoId,
+    createdAt,
+    done: false,
+    attachmentUrl: s3AttachmentUrl,
+    ...newTodo
+  }
+
+  return await todosAcess.createTodoItem(newItem)
+
+}
